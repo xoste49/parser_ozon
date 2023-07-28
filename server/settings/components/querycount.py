7a6f3d3ab75cfb7f3606b2f9fc5380e8458d37cf
Pavel Korchagin <xoste49@gmail.com>
2023-07-28 10:24:30 +0300

    init project

---
+++ server/settings/components/querycount.py
@@ -0,0 +1,10 @@
+QUERYCOUNT = {
+    'THRESHOLDS': {
+        'MEDIUM': 50,
+        'HIGH': 200,
+        'MIN_TIME_TO_LOG': 0,
+        'MIN_QUERY_COUNT_TO_LOG': 0
+    },
+    'IGNORE_REQUEST_PATTERNS': [r'^/admin/', r'^/health/'],
+    'DISPLAY_DUPLICATES': 5,
+}
